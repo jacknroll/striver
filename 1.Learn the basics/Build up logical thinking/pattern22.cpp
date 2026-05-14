756779f0759f0a22cca3007c6a02de8086c9b67f
Yogeshbairwa <ybairwa786@gmail.com>
2026-05-14 13:50:50 +0530

    ...........

--- 1.Learn the basics/Build up logical thinking/pattern22.cpp
+++ 1.Learn the basics/Build up logical thinking/pattern22.cpp
@@ -7,5 +7,10 @@ int main()
 
     for (int i = 0; i < 2 * n - 1; i++)
     {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            cout << (2 * n - 2) - j << " ";
+        }
+        cout << endl;
     }
 }
