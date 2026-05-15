c57ad0102934fd95f67d85fba56b1f9084d4dea8
Yogeshbairwa <ybairwa786@gmail.com>
2026-05-15 14:04:59 +0530

    done primenumber qn

--- 1.Learn the basics/know basic maths/07_primenumberfinding.cpp
+++ 1.Learn the basics/know basic maths/07_primenumberfinding.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+/*
 void isPrime(int n)
 {
-    bool divisior = false;
-    for (int i = 2; i < n; i++)
+    bool divisor = false;
+    if (n = 0 || n == 1)
+        cout << "Non Prime";
+        for (int i = 2; i < n; i++)
     {
         if (n % i == 0)
         {
-            divisior = true;
+            divisor = true;
         }
     }
-    if (divisior == true)
+    if (divisor == true)
+        cout << "Non prime";
+        else
+        cout << "prime";
+    }
+    */
+
+// optimized version
+void isPrime(int n)
+{
+    bool divisor = false;
+    if (n == 0 || n == 1)
+        cout << "Non Prime";
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            divisor = true;
+        }
+    }
+    if (divisor == true)
         cout << "Non prime";
     else
         cout << "prime";
 }
 int main()
 {
-    int n = 11;
+    int n = 2;
     isPrime(n);
     return 0;
 }
