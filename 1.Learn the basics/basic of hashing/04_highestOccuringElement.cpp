5ae95acd78d9771f0434e3df6ff6f3b2ebec5d63
Yogeshbairwa <ybairwa786@gmail.com>
2026-05-22 14:16:37 +0530

    done with solvin problem of finding highest number of frequency

--- 1.Learn the basics/basic of hashing/04_highestOccuringElement.cpp
+++ 1.Learn the basics/basic of hashing/04_highestOccuringElement.cpp
@@ -1,27 +1,53 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include<map>
 using namespace std;
 
+////Using hashing
+/*
 int main()
 {
-    int arr[] = {1, 2, 3, 3, 2, 1, 2,12};
+    int arr[] = {1, 2, 2, 3, 3, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    int max = 0;
+    int maxi = 0;
     for (int i = 0; i < n; i++)
     {
-        if (max < arr[i])
+        if (maxi < arr[i])
         {
-            max = arr[i];
+            maxi = arr[i];
         }
     }
 
     // using hash
-    int hash[max] = {0};
+    int hash[maxi + 1] = {0};
 
     for (int i = 0; i < n; i++)
     {
         hash[arr[i]]++;
     }
 
-    
+    int trackNumber = 0;
+
+    int hashMax = 0;
+    for (int i = 0; i <= maxi; i++)
+    {
+        if (hashMax < hash[i])
+        {
+            hashMax = hash[i];
+            trackNumber = i;
+        }
+    }
+    cout << trackNumber;
+}
+
+*/
+// using mapping
+
+int main()
+{
+    int arr[] = {1, 2, 3, 3, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // declare map
+    map<int, int> map;
 }
