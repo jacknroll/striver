0a6cd8c1f7900912f58833e9f2a38cf57278401a
jacknroll <ybairwa786@gmail.com>
2026-06-05 16:37:23 +0530

    striver.array.Q2-secondlargestElement.cpp

--- Arrays/striver/secondLargest.cpp
+++ Arrays/striver/secondLargest.cpp
@@ -1,14 +1,21 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 
-int secondLargestElement(vector<int> &nums)
+/*
 {
+    int secondLargestElement(vector<int> &nums)
     int secondMax;
     int maxElement;
     if (nums[0] > nums[1])
     {
         maxElement = nums[0];
         secondMax = nums[1];
+    }
+    else if (nums[0] == nums[1])
+    {
+        maxElement = nums[1];
+        secondMax = nums[2];
     }
     else
     {
@@ -22,17 +29,42 @@ int secondLargestElement(vector<int> &nums)
         {
             maxElement = nums[i];
         }
-        else if (nums[i] > secondMax)
+    }
+    for (int i = 2; i < nums.size(); i++)
+    {
+        if (nums[i] > secondMax && nums[i] < maxElement)
         {
             secondMax = nums[i];
         }
     }
     return secondMax;
 }
+*/
+
+int secondLargestElement(vector<int> &arr)
+{
+    sort(arr.begin(), arr.end());
+
+    int largest = arr[arr.size() - 1];
+
+    for (int i = arr.size() - 2; i >= 0; i--)
+    {
+        if (arr[i] != largest)
+        {
+            return arr[i];
+        }
+    }
+    return -1;
+    ;
+}
 
 int main()
 {
-    vector<int> nums = {1, 2, 4, 7,5};
+    vector<int> nums = {
+        8,
+        8,
+        8,
+        8};
     int secondMax = secondLargestElement(nums);
     cout << secondMax;
     return 0;
