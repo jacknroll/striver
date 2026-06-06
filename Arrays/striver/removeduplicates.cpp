8f2012baea27ce27a926b8a1a27802a5e93432fb
jacknroll <ybairwa786@gmail.com>
2026-06-06 18:16:53 +0530

    method of o(n)

--- Arrays/striver/removeduplicates.cpp
+++ Arrays/striver/removeduplicates.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 using namespace std;
 
+/*
 void removeDuplicates(vector<int> arr)
+
 {
 
     for (int i = 0; i < arr.size(); i++)
@@ -19,6 +21,52 @@ void removeDuplicates(vector<int> arr)
     }
 
     for (int x : arr)
+    {
+        cout << x << " ";
+    }
+}
+*/
+
+// do it in o(n'2)
+
+/*
+void removeDuplicates(vector<int> arr)
+
+
+{
+
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            arr.erase(arr.begin() + i);
+            i--;
+        }
+    }
+
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
+}
+*/
+void removeDuplicates(vector<int> arr)
+{
+    vector<int> newArr;
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            newArr.push_back(arr[i]);
+        }
+        else
+        {
+            i++;
+        }
+    }
+    newArr.push_back(arr[arr.size() - 1]);
+
+    for (int x : newArr)
     {
         cout << x << " ";
     }
