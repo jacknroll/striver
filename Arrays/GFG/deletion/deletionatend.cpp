facc9691cfe75706b563480586b10b799921a2ec
jacknroll <ybairwa786@gmail.com>
2026-06-07 18:59:06 +0530

    deleteionatend

---
+++ Arrays/GFG/deletion/deletionatend.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    arr.erase(arr.end());
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
