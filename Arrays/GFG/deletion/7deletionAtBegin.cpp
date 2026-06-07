a3bc89bb218d48d696e8061e37bee8b56511fcb0
jacknroll <ybairwa786@gmail.com>
2026-06-07 18:27:16 +0530

    used shifting logic

--- Arrays/GFG/deletion/7deletionAtBegin.cpp
+++ Arrays/GFG/deletion/7deletionAtBegin.cpp
@@ -3,10 +3,25 @@
 using namespace std;
 
 // built in methods
+/*
+//o(n) ,o(1)
 void removeFirstElement(vector<int> arr)
 {
 
-    arr.erase(arr.begin());
+arr.erase(arr.begin());
+for (int x : arr)
+{
+    cout << x << " ";
+}
+}
+*/
+void removeFirstElement(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr.pop_back();
     for (int x : arr)
     {
         cout << x << " ";
@@ -15,7 +30,7 @@ void removeFirstElement(vector<int> arr)
 
 int main()
 {
-    vector<int> arr = {2, 349, 19, 0};
+    vector<int> arr = {10, 100, 200, 0};
     removeFirstElement(arr);
 
     return 0;
