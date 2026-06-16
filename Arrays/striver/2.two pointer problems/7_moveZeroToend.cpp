3cb60ec4d099d6eea32195556eec09166a3abae0
jacknroll <your@email.com>
2026-06-16 19:30:30 +0530

    just updated the sections nothing else

--- Arrays/striver/2.two pointer problems/7_moveZeroToend.cpp
+++ Arrays/striver/2.two pointer problems/7_moveZeroToend.cpp
@@ -7,15 +7,18 @@ void moveZerosToEnd(vector<int> &nums){
         for(int i=0;i<n;i++){
             if(nums[i]==0){
                 int temp=nums[i];
-                nums[i]=numsuse[n-1];
+                nums[i]=nums[n-1];
                 nums[n-1]=temp;
-               n--
+               n--;
             }
+        }
+        for(int x:nums){
+            cout<<x<<" ";
         }
 }
 
 int main(){
-    vector<int> nums={1,2,3,0,5,5,0,0};
-    moveZeroesToEnd(vector<int> &nums); 
+    vector<int> nums={1,2,3,0,5,5};
+    moveZerosToEnd(nums); 
     return 0;
 }
